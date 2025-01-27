0c73829b2545aeee15cb3ee087aa708f974d1e42
imsykim1130 <imsykim1130@gmail.com>
2025-01-28 02:01:56 +0900

    refactor: Integrate search functionality into RecommendBook component
    - Move search-related state and logic from SearchSection to RecommendBook
    - Add empty click handler to dismiss search results
    - Improve search preview UI and interaction
    - Remove redundant SearchSection component

--- src/pages/common/Landing/RecommendBook.tsx
+++ src/pages/common/Landing/RecommendBook.tsx
@@ -12,6 +12,21 @@ const RecommendBook = () => {
   const [recommendBook, setRecommendBook] = useState<TodayBookInterface | null>(null);
   const navigate = useNavigate();
 
+  const [searchBookList, setSearchBookList] = useState<BookPrevData[]>([]);
+  const [searchWord, setSearchWord] = useState<string>('');
+  const debouncedSearchWord = useDebounce(searchWord, 500);
+
+  // handler: 책 상세 페이지 이동
+  const bookClickHandler = () => {
+    navigate('/book/detail/' + recommendBook?.isbn);
+  };
+
+  // handler: 빈 화면 클릭 시 책 검색 지우기
+  const emptyClickHandler = () => {
+    setSearchBookList([]);
+  };
+
+  // function: 추천 책 받아오기
   const getRecommendBook = () => {
     getRecommendBookRequest().then((res) => {
       if (!res) {
@@ -21,18 +36,76 @@ const RecommendBook = () => {
     });
   };
 
-  const bookClickHandler = () => {
-    navigate('/book/detail/' + recommendBook?.isbn);
+  // function: 책 검색하기
+  const searchBook = () => {
+    const requestDto: getSearchBookListRequestDto = {
+      query: debouncedSearchWord,
+      sort: 'accuracy',
+      page: 1,
+      size: 10,
+      target: 'title',
+    };
+    getSearchBookRequest(requestDto)
+      .then((response) => {
+        // 책 데이터 받아오기 성공
+        const { bookList } = response.data as getSearchBookListResponseDto;
+        setSearchBookList(bookList);
+      })
+      .catch((error) => {
+        // 책 데이터 받아오기 실패
+        console.log(error.response.data);
+      });
   };
 
+  // effect: 추천 책 받아오기
   useEffect(() => {
     getRecommendBook();
   }, []);
 
+  // 검색어 변경 시 책 검색
+  useEffect(() => {
+    if (debouncedSearchWord === '') {
+      setSearchBookList([]);
+      return;
+    }
+    searchBook();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedSearchWord]);
+
   return (
-    <section className="relative flex flex-col gap-[60px] justify-center items-center pt-[20vh] pb-[15vh]">
+    <section
+      className="relative flex flex-col gap-[60px] justify-center items-center pt-[20vh] pb-[15vh]"
+      onClick={emptyClickHandler}
+    >
       {/* 검색 */}
-      <SearchSection />
+      <div className={'absolute top-[60px] w-[25rem]'}>
+        <SearchBox searchWord={searchWord} setSearchWord={setSearchWord} />
+        {/* 검색 미리보기 */}
+        {searchBookList.length ? (
+          <div
+            className={
+              'absolute z-50 w-full h-[400px] p-[5px] top-[70px] left-1/2 -translate-x-1/2 flex flex-col bg-white rounded-[10px] drop-shadow-md overflow-scroll scroll-smooth'
+            }
+          >
+            {searchBookList.map((book) => (
+              <div key={book.isbn} className={'rounded-[10px] p-[15px] hover:bg-black hover:bg-opacity-5 duration-200'}>
+                <div className={'w-[100px]'}>
+                  <BookPrev
+                    bookImg={book.bookImg}
+                    author={book.author}
+                    title={book.title}
+                    isbn={book.isbn}
+                    imgSize={3}
+                  />
+                </div>
+              </div>
+            ))}
+            <button className={'font-semibold py-[10px] hover:bg-black hover:bg-opacity-5 rounded-[10px]'}>
+              검색결과 더보기
+            </button>
+          </div>
+        ) : null}
+      </div>
       <div className={'flex flex-col gap-[30px]'}>
         {/* 배경 이미지 */}
         <div className={'absolute top-0 left-0 w-full -z-10 blur-2xl'}>
@@ -68,66 +141,6 @@ const RecommendBook = () => {
         </div>
       </div>
     </section>
-  );
-};
-
-const SearchSection = () => {
-  const [searchBookList, setSearchBookList] = useState<BookPrevData[]>([]);
-  const [searchWord, setSearchWord] = useState<string>('');
-  const debouncedSearchWord = useDebounce(searchWord, 500);
-
-  // function: 책 검색하기
-  const searchBook = () => {
-    const requestDto: getSearchBookListRequestDto = {
-      query: debouncedSearchWord,
-      sort: 'accuracy',
-      page: 1,
-      size: 10,
-      target: 'title',
-    };
-    getSearchBookRequest(requestDto)
-      .then((response) => {
-        // 책 데이터 받아오기 성공
-        const { bookList } = response.data as getSearchBookListResponseDto;
-        setSearchBookList(bookList);
-      })
-      .catch((error) => {
-        // 책 데이터 받아오기 실패
-        console.log(error.response.data);
-      });
-  };
-
-  // 검색어 변경 시 책 검색
-  useEffect(() => {
-    if (debouncedSearchWord === '') {
-      setSearchBookList([]);
-      return;
-    }
-    searchBook();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [debouncedSearchWord]);
-
-  return (
-    <div className={'absolute top-[60px]'}>
-      <SearchBox searchWord={searchWord} setSearchWord={setSearchWord} />
-      {/* 검색 미리보기 */}
-      {searchBookList.length ? (
-        <div
-          className={
-            'absolute z-50 w-full h-[400px] p-[5px] top-[70px] left-1/2 -translate-x-1/2 flex flex-col bg-white rounded-[10px] drop-shadow-md overflow-scroll scroll-smooth'
-          }
-        >
-          {searchBookList.map((book) => (
-            <div key={book.isbn} className={'rounded-[10px] p-[15px] hover:bg-black hover:bg-opacity-5 duration-200'}>
-              <BookPrev bookImg={book.bookImg} author={book.author} title={book.title} isbn={book.isbn} imgSize={70} />
-            </div>
-          ))}
-          <button className={'font-semibold py-[10px] hover:bg-black hover:bg-opacity-5 rounded-[10px]'}>
-            검색결과 더보기
-          </button>
-        </div>
-      ) : null}
-    </div>
   );
 };
 
