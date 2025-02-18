aee75ed35b013c9e26948ebeef8fc8b5fe67b669
imsykim1130 <imsykim1130@gmail.com>
2025-02-19 03:18:03 +0900

    feat: 배송정보 가져오기 useQuery 생성

--- src/api/query.ts
+++ src/api/query.ts
@@ -1,6 +1,8 @@
 import { useQuery, UseQueryOptions } from 'react-query';
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import { DOMAIN } from '@/utils';
+import { GetAllDeliveryInfoResponseDto } from '@/api/response.dto.ts';
+import { DeliveryInfoItem } from '@/api/item.ts';
 
 export const allCartBookKey = 'allCartBook';
 export const useAllCartBookQuery = <T>(
@@ -20,6 +22,31 @@ export const useAllCartBookQuery = <T>(
       select: (result): T => {
         return result.data;
       },
+      ...opt,
+    },
+  );
+};
+
+export const allDeliveryInfoKey = ['allDeliveryInfo'];
+export const useAllDeliveryInfoQuery = (
+  jwt: string,
+  opt?: UseQueryOptions<AxiosResponse, AxiosError, DeliveryInfoItem[], typeof allDeliveryInfoKey>,
+) => {
+  return useQuery(
+    allDeliveryInfoKey,
+    async () => {
+      return await axios.get(DOMAIN + '/user/delivery-info/all', {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      });
+    },
+    {
+      select: (response) => {
+        const { userDeliveryInfoList } = response.data as GetAllDeliveryInfoResponseDto;
+        return userDeliveryInfoList;
+      },
+      staleTime: Infinity,
       ...opt,
     },
   );
