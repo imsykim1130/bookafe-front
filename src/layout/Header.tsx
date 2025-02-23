4510412ba57d9469c6d093a40f203a67f8a09829
imsykim1130 <imsykim1130@gmail.com>
2025-02-24 02:26:53 +0900

    fix: header 네비게이션 수정
    - 모바일 환경에서 네비게이션이 드롭다운 형식으로 변경되도록 수정
    - 알람 아이콘 추가

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,27 +1,33 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react';
+import { useCookies } from 'react-cookie';
 import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
 import { useUserStore } from '../zustand/userStore';
-import { useCookies } from 'react-cookie';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const [, , removeCookie] = useCookies(['jwt']);
+
   const { pathname } = useLocation();
   const isAuthPage = pathname.includes('/auth/');
   const { authType } = useParams();
+
   const { user } = useUserStore();
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-  const navigate = useNavigate();
-  const [profileImageUrl, setProfileImageUrl] = useState<string | null>(null);
-  const [, , removeCookie] = useCookies(['jwt']);
 
-  // function: 이미지 url 생성
-  const changeProfileImage = (changeImage: string) => {
-    if (!changeImage) {
-      setProfileImageUrl(null);
-      return;
-    }
-    setProfileImageUrl(`http://localhost:8080/image/${changeImage}`);
-  };
+  const [isAlarm, setIsAlarm] = useState<boolean>(false);
+
+  const [isNavOpened, setIsNavOpened] = useState<boolean>(false);
+  // const [profileImageUrl, setProfileImageUrl] = useState<string | null>(null);
+
+  // // function: 이미지 url 생성
+  // const changeProfileImage = (changeImage: string) => {
+  //   if (!changeImage) {
+  //     setProfileImageUrl(null);
+  //     return;
+  //   }
+  //   setProfileImageUrl(`http://localhost:8080/image/${changeImage}`);
+  // };
 
   // function: 로그아웃 버튼 클릭 핸들러
   function logoutClickHandler() {
@@ -34,11 +40,16 @@ const Header = () => {
     navigate('/auth/sign-in', { state: { pathname: pathname } });
   }
 
-  // effect: 프로필 이미지 변경 시 새로 받아오기
-  useEffect(() => {
-    if (!user) return;
-    changeProfileImage(user.profileImg);
-  }, [user?.profileImg]);
+  // function: 네비게이션 열기/닫기
+  function toggleNav() {
+    setIsNavOpened(!isNavOpened);
+  }
+
+  // // effect: 프로필 이미지 변경 시 새로 받아오기
+  // useEffect(() => {
+  //   if (!user) return;
+  //   changeProfileImage(user.profileImg);
+  // }, [user?.profileImg]);
 
   // effect: user 변경 사항으로 로그인 여부 확인
   useEffect(() => {
@@ -49,92 +60,57 @@ const Header = () => {
     setIsLoggedIn(true);
   }, [user]);
 
-  // todo: 테스트용 삭제 필요
-  useEffect(() => {
-    if (!user) return;
-    console.log(user.email);
-  }, []);
-
   return (
     <header
-      className={`sticky z-40 top-0 w-full flex justify-between items-center px-[5%] md:px-[10%] lg:px-[15%] py-5 bg-white shadow-[0_0_4px_rgba(0,0,0,0.1)] text-xs`}
+      className={`sticky z-40 top-0 w-full flex justify-between items-center px-[5vw] md:px-[10vw] py-5 bg-white shadow-[0_0_4px_rgba(0,0,0,0.1)] text-xs`}
     >
       {/*로고*/}
       <Link to="/" className="w-[30px] h-[30px] flex justify-center items-center">
         <i className="fi fi-ss-book-alt text-[px]"></i>
       </Link>
+
       {/*네비게이션*/}
       <nav className="relative flex gap-[2rem] md:gap-[3rem] items-center">
-        {/*장바구니*/}
-        {!isAuthPage && user && user.role !== 'ROLE_ADMIN' ? (
-          <Link to="/cart" className="w-[30px] h-[30px] flex justify-center items-center cursor-pointer">
-            <i className="fi fi-rr-shopping-cart text-[15px] md:hidden"></i>
-            <p className="hidden md:block text-nowrap">장바구니</p>
-          </Link>
-        ) : (
-          ''
-        )}
-        {/*좋아요*/}
-        {!isAuthPage && user && user.role !== 'ROLE_ADMIN' ? (
-          <Link to={'/favorite'} className="w-[30px] h-[30px] flex justify-center items-center cursor-pointer">
-            <i className="fi fi-rs-heart text-[15px]  md:hidden"></i>
-            <p className="hidden md:block text-nowrap">좋아요</p>
-          </Link>
-        ) : (
-          ''
-        )}
-        {/*유저*/}
-        {isLoggedIn && user && user.role !== 'ROLE_ADMIN' ? (
-          <Link
-            to={'/user'}
-            className="w-[30px] h-[30px] rounded-full flex items-center justify-center overflow-hidden"
-          >
-            <div className={'w-[20px] h-[20px] rounded-full flex items-center justify-center overflow-hidden'}>
-              {profileImageUrl ? (
-                <img src={profileImageUrl} alt="profile img" />
-              ) : (
-                <i className="fi fi-rr-user text-[15px] flex items-center justify-center"></i>
-              )}
-            </div>
-          </Link>
-        ) : (
-          ''
-        )}
+        {/* 알림 버튼 */}
+        <button className="relative">
+          <i className="flex items-center justify-center text-base fi fi-ss-bell"></i>
+          {isAlarm && (
+            <span className="absolute block -translate-x-1/2 bg-red-500 rounded-full size-[0.3125rem] top-5 left-1/2"></span>
+          )}
+        </button>
+        {/* 메뉴 버튼 */}
+        <button className="md:hidden" onClick={toggleNav}>
+          <i className="flex items-center justify-center text-base fi fi-br-grid"></i>
+        </button>
 
-        {/* 로그인 */}
-        {(!isAuthPage && !user) || (isAuthPage && authType === 'sign-up') ? (
-          <div
-            className="w-[30px] h-[30px] flex justify-center items-center cursor-pointer"
-            onClick={signInClickHandler}
-          >
-            <i className="fi fi-rr-insert-alt text-[15px] flex items-center justify-center md:hidden"></i>
-            <p className="hidden md:block text-nowrap">로그인</p>
-          </div>
-        ) : (
-          ''
-        )}
-        {/* 회원가입 */}
-        {isAuthPage && !user && authType === 'sign-in' ? (
-          <Link to={'/auth/sign-up'} className="w-[30px] h-[30px] flex justify-center items-center cursor-pointer">
-            <i className="fi fi-rr-user-add text-[15px] flex items-center justify-center md:hidden"></i>
-            <p className="hidden md:block text-nowrap">회원가입</p>
-          </Link>
-        ) : (
-          ''
-        )}
-
-        {user && user.role === 'ROLE_ADMIN' ? (
-          <button
-            className="flex items-center justify-center cursor-pointer"
-            onClick={() => {
-              logoutClickHandler();
-            }}
-          >
-            <p className="text-nowrap">로그아웃</p>
-          </button>
-        ) : (
-          ''
-        )}
+        <div className={`items-start gap-[1.875rem] nav ${isNavOpened ? 'opacity-100' : 'opacity-0'}`}>
+          {!isLoggedIn ? (
+            <>
+              {/* 로그인 안되어 있을 때 */}
+              {/* 로그인, 로그아웃 */}
+              <Link to={'/auth/sign-in'} className="text-nowrap hover:opacity-70" onClick={signInClickHandler}>
+                로그인
+              </Link>
+              <Link to={'/auth/sign-up'} className="text-nowrap hover:opacity-70">
+                회원가입
+              </Link>
+            </>
+          ) : (
+            <>
+              {/* 로그인 되어 있을 때 */}
+              {/* 좋아요, 내 정보, 로그아웃 */}
+              <Link to={'/favorite'}>좋아요</Link>
+              <Link to={'/user'}>내 정보</Link>
+              <button
+                onClick={() => {
+                  logoutClickHandler();
+                }}
+              >
+                로그아웃
+              </button>
+            </>
+          )}
+        </div>
       </nav>
     </header>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './index.css';
+import './App.css';
 
 import App from './App.tsx';
 import Search from './pages/Search.tsx';
