2bfb7c55eeb511847460155818882456b79def33
imsykim1130 <imsykim1130@gmail.com>
2025-02-25 14:43:22 +0900

    fix: 유저 정보 useQuery 로 가져오는 것으로 수정
    - 캐시값 이용을 위해 useQuery 로 가져옴

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,46 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from 'react';
+import { useCookies } from 'react-cookie';
 import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { userInfoKey, useUserInfoQuery } from './api/query.ts';
 import './App.css';
 import Footer from './layout/Footer.tsx';
 import Header from './layout/Header.tsx';
-
-import { useCookies } from 'react-cookie';
-import { getUserRequest } from './api/api.ts';
-import { GetUserResponseDto, ResponseDto } from './api/response.dto.ts';
 import { useUserStore } from './zustand/userStore.ts';
+import { useQueryClient } from '@tanstack/react-query';
+
 
 const App = () => {
   const { pathname } = useLocation();
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const [cookies] = useCookies(['jwt']);
-  const { user, loadingUser, errorUser, updateUser, resetUser } = useUserStore();
+  const { data: userInfo, refetch: refetchUserInfo } = useUserInfoQuery(cookies.jwt);
+  const { resetUser, updateUser, loadingUser } = useUserStore();
 
-  // function: zustand userStore 에 유저 정보 가져오기
-  const zustandGetUserStore = () => {
-    // 로딩 표시
-    loadingUser();
-    getUserRequest(cookies.jwt)
-      .then((res) => {
-        // 성공
-        console.log('zustand user 정보 가져오기 성공');
-        const { user, totalPoint } = res.data as GetUserResponseDto;
-        updateUser(user, totalPoint);
-      })
-      .catch((err) => {
-        // 실패
-        // 에러 응답이 아예 안왔을 때
-        if (!err) {
-          window.alert('네트워크 에러');
-          errorUser();
-          return;
-        }
-        // 에러 응답 콘솔 출력
-        const { message } = err.response.data as ResponseDto;
-        console.log('유저 정보 받아오기 에러 발생 : ' + message);
-        errorUser();
-      });
-  };
-
-  // effect: 쿠키 변경 시 jwt 가 없으면 userStore 비우기
+  // effect: 쿠키 변경 시 jwt 가 없으면 userStore 비우기, 쿼리 무효화
   // jwt 가 있으면 새로운 user 값 useStore 로 가져오기
   useEffect(() => {
-    if (!cookies.jwt && user) {
+    if (cookies.jwt === undefined) {
       resetUser();
+      queryClient.invalidateQueries({
+        queryKey:[userInfoKey]
+      })
       navigate('/auth/sign-in');
       return;
     }
-
-    if (cookies.jwt && !user) {
-      zustandGetUserStore();
-    }
+    refetchUserInfo();
+    loadingUser();
   }, [cookies]);
+
+  // effect
+  useEffect(() => {
+    if (userInfo === undefined) {
+      resetUser();
+      return;
+    }
+    updateUser(userInfo.user, userInfo.totalPoint);
+  }, [userInfo]);
 
   // effect: 페이지 이동 시 스크롤 맨 위로 이동
   useEffect(() => {
