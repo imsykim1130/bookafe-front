ecef4c6cafda9c493ab5199c1241bd6751f88e63
imsykim1130 <imsykim1130@gmail.com>
2025-03-16 19:12:08 +0900

    fix: 로그인 알림 메시지 수정
    - 401 에러 발생 시 사용자에게 보여주는 로그인 알림 메시지를 간소화하여 '다시 로그인 해주세요.'로 변경

--- src/api/template.ts
+++ src/api/template.ts
@@ -10,7 +10,7 @@ axios.interceptors.response.use(
     if (error.response && error.response.status === 401) {
       // 에러가 401 이면 jwt 만료로 인식
       // 로그인 페이지로 이동
-      window.alert('로그인이 필요합니다. 다시 로그인 해주세요.');
+      window.alert('다시 로그인 해주세요.');
       window.location.href = '/auth/sign-in?logout=true';
     }
     return error;
