b6184303b6626a95f767c6483dac451856c7dd46
imsykim1130 <imsykim1130@gmail.com>
2025-05-12 13:38:07 +0900

    feat: 사용자 언로드 시 구독 해제 처리
    - 로그아웃 이외에 윈도우를 닫을 때도 sse 연결이 종료되었다는 것을 알리기 위해 sendBeacon 으로 sse 구독 해제 api 요청 전송

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,39 @@
-import { useEffect } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import './App.css';
 import Footer from './layout/Footer.tsx';
 import Header from './layout/Header.tsx';
 import { Toaster } from './components/ui/toaster.tsx';
+import { DOMAIN } from './utils/env.ts';
+import { useEffect } from 'react';
+import { useUserQuery } from './hook/user.hook.ts';
 
 const App = () => {
   const { pathname } = useLocation();
-  
+  const { user } = useUserQuery();
+
+  const handleBeforeUnload = (userId: number) => {
+      const body = {userId: userId};
+      const blob = new Blob([JSON.stringify(body)], { type: 'application/json' });
+      navigator.sendBeacon(DOMAIN + '/sse/unsubscribe', blob);
+
+      // event.preventDefault();
+      // event.returnValue = ''; // This empty string is needed for some older browsers
+  }
+
   // effect: 페이지 이동 시 스크롤 맨 위로 이동
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
+
+  useEffect(() => {
+    if(!user) return;
+    window.addEventListener('beforeunload', () => handleBeforeUnload(user.id));
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('beforeunload', () => handleBeforeUnload(user.id));
+    }
+  }, [user]);
   
   // render
   return (
